14a2690ae62cefed96848940970f6bead0809247
josefdzbeta <jafb19@outlook.com>
2022-02-18 01:20:57 +0100

    v5

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -35,10 +35,10 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
   
-  onSelect (hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Héroe Seleccionado id=${hero.id}`);
-  }
+  // onSelect (hero: Hero): void{
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Héroe Seleccionado id=${hero.id}`);
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes()
